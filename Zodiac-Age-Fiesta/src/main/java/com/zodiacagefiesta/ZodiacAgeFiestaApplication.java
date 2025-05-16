0d00041e079b9c2bb29e33ba1696189bcadcd336
Jungmin Moon <jmmoon96@gmail.com>
2025-05-15 23:26:27 -0400

    Initial

---
+++ Zodiac-Age-Fiesta/src/main/java/com/zodiacagefiesta/ZodiacAgeFiestaApplication.java
@@ -0,0 +1,13 @@
+package com.zodiacagefiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacAgeFiestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZodiacAgeFiestaApplication.class, args);
+	}
+
+}
---
+++ Zodiac-Age-Fiesta/src/test/java/com/zodiacagefiesta/ZodiacAgeFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zodiacagefiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZodiacAgeFiestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
