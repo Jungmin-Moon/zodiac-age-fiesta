aa690ec34001ef1d27322fa96f8fb84c85c61902
Jungmin Moon <jmmoon96@gmail.com>
2025-08-03 01:57:46 -0400

    Need to fix account management, users do not get added to the db

--- Zodiac-Age-Fiesta/src/main/java/com/zodiacagefiesta/controllers/HomeController.java
+++ Zodiac-Age-Fiesta/src/main/java/com/zodiacagefiesta/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.zodiacagefiesta.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("home")
@@ -10,7 +11,16 @@ public class HomeController {
 
 	
 	@GetMapping
-	public String home() {
+	public String home(@RequestParam(required = false) String login, @RequestParam(required = false) String register) {
+		
+		if (login != null) {
+			return "redirect:/login";
+		} 
+		
+		if (register != null) {
+			return "redirect:/register";
+		}
+		
 		return "home.html";
 	}
 }
