1e0947d5dbd04571e027cb1fafdd9dad1cea08c9
Jungmin Moon <jmmoon96@gmail.com>
2025-08-03 02:13:48 -0400

    I am dumb and didn't have the controller actually do anything

--- Zodiac-Age-Fiesta/src/main/java/com/zodiacagefiesta/controllers/RegisterController.java
+++ Zodiac-Age-Fiesta/src/main/java/com/zodiacagefiesta/controllers/RegisterController.java
@@ -1,8 +1,12 @@
 package com.zodiacagefiesta.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zodiacagefiesta.entities.User;
 
 @Controller
 @RequestMapping("/register")
@@ -12,5 +16,12 @@ public class RegisterController {
 	@GetMapping()
 	public String register() {
 		return "register";
+	}
+	
+	@PostMapping() 
+	public String registerPost(User user, Model model) {
+		
+		
+		
 	}
 }
